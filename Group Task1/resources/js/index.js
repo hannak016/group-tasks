076bfa71dcd912a693b10d30f7c7cd53ac333ea1
Zihan Kong <zihan.kong@campus.lmu.de>
2020-12-10 15:02:24 +0800

    cleared up

--- Group Task1/resources/js/index.js
+++ Group Task1/resources/js/index.js
@@ -6,37 +6,20 @@
 
 /* render input value */
 
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
 
-const img = new Image();
-img.src = "./resources/img/1.png";
-img.onload = () => {
-  context.drawImage(img, 0, 0, 150, 200);
-};
 
-const topInput = document.getElementById("top");
-const bottomInput = document.getElementById("bottom");
+const topInput = document.querySelector(".top input");
+const bottomInput = document.querySelector(".bottom input");
+const topTextarea = document.querySelector(".topTextarea");
+const bottomTextarea  = document.querySelector(".bottomTextarea");
 
-context.font = "30px Comic Sans MS";
-context.textAlign = "center";
+const imgBox = document.querySelector(".img-box img");
 
-let bottomText = "";
-let topText = "";
+
 
 topInput.addEventListener("input", (e) => {
-  topText = e.target.value;
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(img, 0, 0, 300, 500);
-  context.fillText(topText, 150, 50);
-  context.fillText(bottomText, 150, 450);
+  topTextarea.innerHTML = e.target.value;
 });
 bottomInput.addEventListener("input", (e) => {
-  bottomText = e.target.value;
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(img, 0, 0, 300, 500);
-  context.fillText(topText, 150, 50);
-  context.fillText(bottomText, 150, 450);
+  bottomTextarea.innerHTML = e.target.value;
 });
