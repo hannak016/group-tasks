381d08e5f0fdc6bf9b66a2d1eb84b5121e8826e2
Zihan Kong <zihan.kong@campus.lmu.de>
2020-12-10 15:38:00 +0800

    layout

--- Group Task1/resources/js/index.js
+++ Group Task1/resources/js/index.js
@@ -1,21 +1,7 @@
-//TODO
-
-/* get element */
-
-/* attach event*/
-
-/* render input value */
-
-
-
 const topInput = document.querySelector(".top input");
 const bottomInput = document.querySelector(".bottom input");
 const topTextarea = document.querySelector(".topTextarea");
-const bottomTextarea  = document.querySelector(".bottomTextarea");
-
-const imgBox = document.querySelector(".img-box img");
-
-
+const bottomTextarea = document.querySelector(".bottomTextarea");
 
 topInput.addEventListener("input", (e) => {
   topTextarea.innerHTML = e.target.value;
@@ -23,3 +9,20 @@ topInput.addEventListener("input", (e) => {
 bottomInput.addEventListener("input", (e) => {
   bottomTextarea.innerHTML = e.target.value;
 });
+
+const btnPrev = document.querySelector(".btnPrev");
+const btnNext = document.querySelector(".btnNext");
+let curImg = document.getElementsByTagName("img")[0];
+
+const min = 1;
+const max = 4;
+let curIndex = min;
+
+btnPrev.onclick = function () {
+  curIndex === min ? (curIndex = max) : curIndex--;
+  curImg.setAttribute("src", `resources/img/${curIndex}.JPG`);
+};
+btnNext.onclick = function () {
+  curIndex === max ? (curIndex = min) : curIndex++;
+  curImg.setAttribute("src", `resources/img/${curIndex}.JPG`);
+};
